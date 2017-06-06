4b04c5a6dea7b86854ffdb82a07018ed4b4efa30
Cacoguaa <cacoguaa@unal.edu.co>
2017-06-05 19:29:22 -0500

    Update ui

--- src/view/MainPanel.java
+++ src/view/MainPanel.java
@@ -17,14 +17,18 @@ import java.util.List;
 import javax.swing.JTextField;
 
 import test.GLCP;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class MainPanel {
 
 	private JFrame frame;
 
 	private String text;
+	private String base;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
 	/**
 	 * Launch the application.
 	 */
@@ -70,15 +74,36 @@ public class MainPanel {
 				}
 			}
 		});
-		btnNewButton.setBounds(44, 274, 89, 23);
+		btnNewButton.setBounds(42, 180, 89, 23);
 		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnEntrenar = new JButton("Entrenar");
+		btnEntrenar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser chooser = new JFileChooser();
+				frame.getContentPane().add(chooser, BorderLayout.SOUTH);
+				int returnVal = chooser.showOpenDialog(frame);
+				if(returnVal == JFileChooser.APPROVE_OPTION) {
+					base = chooser.getSelectedFile().getPath();
+					textField_2.setText(base);
+				}
+			}
+		});
+		btnEntrenar.setBounds(42, 107, 89, 23);
+		frame.getContentPane().add(btnEntrenar);
+		
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		textField_2.setColumns(10);
+		textField_2.setBounds(42, 141, 366, 20);
+		frame.getContentPane().add(textField_2);
 		
 		JButton btnNewButton_1 = new JButton("Calcular");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String base = "C:\\Users\\Ace\\Documents\\Workspace\\Estocasticos\\";
+
 				List<String> booksForTraining = new ArrayList<String>();
-				booksForTraining.add(base + "src\\examples\\basico.py");
+				booksForTraining.add(base);
 				booksForTraining.add(text);
 				//booksForTraining.add(base + "src\\examples\\MONTE.py");
 				GLCP glcp = null;
@@ -90,7 +115,7 @@ public class MainPanel {
 				}
 				double result = 0;
 				try {
-					result = glcp.getProb(base + "src\\examples\\basico.py");
+					result = glcp.getProb(base);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -99,20 +124,27 @@ public class MainPanel {
 				textField_1.setText(String.valueOf(result));
 			}
 		});
-		btnNewButton_1.setBounds(218, 274, 89, 23);
+		btnNewButton_1.setBounds(42, 245, 89, 23);
 		frame.getContentPane().add(btnNewButton_1);
 		
 		textField = new JTextField();
 		textField.setEditable(false);
-		textField.setBounds(44, 92, 366, 20);
+		textField.setBounds(42, 214, 366, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setEditable(false);
 		textField_1.setColumns(10);
-		textField_1.setBounds(44, 153, 366, 20);
+		textField_1.setBounds(42, 285, 366, 20);
 		frame.getContentPane().add(textField_1);
+		
+		
+		
+		JLabel lblAnalizadorDeGramaticas = new JLabel("Analizador De Gramaticas Probabilisticas Libres De Contexto");
+		lblAnalizadorDeGramaticas.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAnalizadorDeGramaticas.setBounds(42, 21, 366, 60);
+		frame.getContentPane().add(lblAnalizadorDeGramaticas);
 	}
 
 	public String getText() {
